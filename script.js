0de929f001654980b1573493df8b95836ac0a9c1
Ryan <65420906+Ryan-Webb62@users.noreply.github.com>
2021-01-23 10:11:21 -0500

    Implement new game button

--- script.js
+++ script.js
@@ -12,15 +12,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//  Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//  Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score1El.textContent = 0;
+  score0El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0EL.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0EL.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  diceEl.classList.add('hidden');
+};
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -29,6 +40,9 @@ const switchPlayer = function () {
   player0EL.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+// Start Game
+init();
 
 //  Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -77,3 +91,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
